5e13fcafbacb8881bf0dcd2ac6e30387baa51bd8
bloodseekeers <52128528+bloodseekeers@users.noreply.github.com>
2020-07-27 21:42:59 +0200

    Add files via upload

---
+++ docs/precache-manifest.41316634d87a4b09fff46cbf72c3ea4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90b861518f7fc4eca429341b9824428e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a876f8ab52fc44bb3db",
+    "url": "/static/css/2.5e652310.chunk.css"
+  },
+  {
+    "revision": "78991bd168e4fdbded38",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3a876f8ab52fc44bb3db",
+    "url": "/static/js/2.645dcd51.chunk.js"
+  },
+  {
+    "revision": "fcca2cdcc5e6967026a1ba00172f2d47",
+    "url": "/static/js/2.645dcd51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78991bd168e4fdbded38",
+    "url": "/static/js/main.7a1ac507.chunk.js"
+  },
+  {
+    "revision": "6de13c8d612ede655b13",
+    "url": "/static/js/runtime-main.77df2d57.js"
+  }
+]);
